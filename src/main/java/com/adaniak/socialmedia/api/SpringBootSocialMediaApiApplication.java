26fdd2d4fef65dd7a43d201c7043ba343d6ca809
Süleyman Şenadım <suleyman@senadim.com>
2022-12-27 23:49:50 +0300

    First commit.

---
+++ src/main/java/com/adaniak/socialmedia/api/SpringBootSocialMediaApiApplication.java
@@ -0,0 +1,13 @@
+package com.adaniak.socialmedia.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSocialMediaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSocialMediaApiApplication.class, args);
+	}
+
+}
