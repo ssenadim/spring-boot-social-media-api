7c6572942357a003dca8d49eb827c928701b6144
Süleyman Şenadım <suleyman@senadim.com>
2022-12-29 00:41:39 +0300

    Accept Language

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # spring-boot-social-media-api
 
-Rest Api Postman Collection https://github.com/ssenadim/spring-boot-social-media-api/blob/main/Social-Media-Api.postman_collection.json
+Rest Api Postman
+Collection https://github.com/ssenadim/spring-boot-social-media-api/blob/main/Social-Media-Api.postman_collection.json
+---
+#Readme Note 1
+Swagger UI      : http://localhost:1903/swagger-ui.html
+Open API Spec   : http://localhost:1903/v3/api-docs
+
+<dependency>
+    <groupId>org.springdoc</groupId>
+    <artifactId>springdoc-openapi-starter-webmvc-ui</artifactId>
+    <version>2.0.2</version>
+</dependency>
+
+---
+#Readme Note 2
+Different Representations are possible
+Example: Different Content Type - XML or JSON or ...
+Example: Different Language - English or Dutch or ...
+
+Content Negotiation
+Example: Accept header (MIME types - application/xml, application/json ...)
+Example: Accept-Language header (en,nl,fr, ...)
+
+<dependency>
+    <groupId>com.fasterxml.jackson.dataformat</groupId>
+    <artifactId>jackson-dataformat-xml</artifactId>
+</dependency>
+
+---
--- src/main/java/com/adaniak/socialmedia/api/user/UserResource.java
+++ src/main/java/com/adaniak/socialmedia/api/user/UserResource.java
@@ -2,19 +2,34 @@ package com.adaniak.socialmedia.api.user;
 
 import com.adaniak.socialmedia.api.exception.UserNotFoundException;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 public class UserResource {
+    private MessageSource messageSource;
     private UserDaoService service;
 
-    public UserResource(UserDaoService service) {
+    public UserResource(UserDaoService service, MessageSource messageSource) {
         this.service = service;
+        this.messageSource = messageSource;
+    }
+
+    @GetMapping("/welcome/{id}")
+    public String welcomeMessageForUser(@PathVariable int id) {
+        User user = service.findById(id);
+        if (user == null)
+            throw new UserNotFoundException("id:" + id);
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("welcome.message", null, "Welcome", locale) + " " + user.getName();
     }
 
     @GetMapping("/users")
